6f93787b1f469c1f956592c39335fe4132478516
tactics177 <63974953+tactics177@users.noreply.github.com>
2025-03-03 12:28:52 +0100

    babel

--- header/webpack.config.js
+++ header/webpack.config.js
@@ -2,27 +2,44 @@ import { ModuleFederationPlugin } from '@module-federation/enhanced/webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
-  mode: "development",
-  devServer: {
-    port: 3001,
-  },
-  output: {
-    publicPath: "http://localhost:3001/",
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "header",
-      filename: "remoteEntry.js",
-      exposes: {
-        "./Header": "./src/Header",
-      },
-      shared: {
-        react: { singleton: true },
-        "react-dom": { singleton: true },
-      },
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-  ],
+    mode: "development",
+    devServer: {
+        port: 3001,
+    },
+    output: {
+        publicPath: "http://localhost:3001/",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"],
+                    },
+                },
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".js", ".jsx"],
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: "header",
+            filename: "remoteEntry.js",
+            exposes: {
+                "./Header": "./src/Header",
+            },
+            shared: {
+                react: { singleton: true },
+                "react-dom": { singleton: true },
+            },
+        }),
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+        }),
+    ],
 };
--- shell/webpack.config.js
+++ shell/webpack.config.js
@@ -2,28 +2,43 @@ import { ModuleFederationPlugin } from '@module-federation/enhanced/webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
-  mode: "development",
-  devServer: {
-    port: 3000,
-  },
-  output: {
-    publicPath: "http://localhost:3000/",
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "shell",
-      remotes: [
-        {
-          header: "header@http://localhost:3001/remoteEntry.js",
-        },
-      ],
-      shared: {
-        react: { singleton: true },
-        "react-dom": { singleton: true },
-      },
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-  ],
+    mode: "development",
+    devServer: {
+        port: 3000,
+    },
+    output: {
+        publicPath: "http://localhost:3000/",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"],
+                    },
+                },
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".js", ".jsx"],
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: "shell",
+            remotes: {
+                header: "header@http://localhost:3001/remoteEntry.js",
+            },
+            shared: {
+                react: { singleton: true },
+                "react-dom": { singleton: true },
+            },
+        }),
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+        }),
+    ],
 };
